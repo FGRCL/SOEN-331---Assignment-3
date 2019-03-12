7184022f0580c55ca6166d0cffa02c6d9ca369bf
FGRCL <35940434+FGRCL@users.noreply.github.com>
2019-03-12 10:27:46 -0400

    Added heap methods

--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -1,9 +1,29 @@
 
 public class PriorityQueue<K extends Comparable<K>, V> {
 	private Node<K, V>[] heap;
+	private int nbElements;
+	
+	private class Node<K extends Comparable<K>, V>{
+		private K key;
+		private V value;
+		
+		public Node(K key, V value) {
+			this.key = key;
+			this.value = value;
+		}
+		
+		public K getKey() {
+			return key;
+		}
+		
+		public V getValue() {
+			return value;
+		}
+	}
+
 	
 	private void insert(K key, V value) {
-		
+		Node insertNode = new Node(key, value);
 	}
 	
 	private V remove() {
@@ -11,5 +31,23 @@ public class PriorityQueue<K extends Comparable<K>, V> {
 	
 	private V min() {
 		
+	}
+	
+	private int parent(int i) { 
+		return ((i-1)/2);
+	}
+	
+	private int leftChild(int i) {
+		return ((2*i)+1);
+	}
+	
+	private int rightChild(int i) {
+		return ((2*i)+2);
+	}
+	
+	private void swap(int a, int b) {
+		Node temp = heap[a];
+		heap[a] = heap[b];
+		heap[b] = temp;
 	}
 }
