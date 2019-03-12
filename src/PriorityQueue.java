5453716e14194d7c8f493456ecf2ecb850cca615
Brianne O'Gallagher-Roy <35981744+bogalla@users.noreply.github.com>
2019-03-12 14:30:55 -0400

    Contracts improved (#4)
    * made bri's contains method
    
    * contracts basics done
    
    * made franky's constructor
    
    * made franky's constructor without parameters
    
    * invariant
    
    * move constructor
    
    * revisisons
    
    * bracket

--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -1,10 +1,14 @@
 import be.ac.ua.ansymo.adbc.annotations.ensures;
+import be.ac.ua.ansymo.adbc.annotations.invariant;
 import be.ac.ua.ansymo.adbc.annotations.requires;
 
+@invariant ({	"$this.nbElements <= $this.getSize()",
+				"$this.nbElements >= 0"
+			})
 public class PriorityQueue<K extends Comparable<K>, V> {
 	private Node<K, V>[] heap;
 	private int nbElements;
-	
+
 	private class Node<K extends Comparable<K>, V>{
 		private K key;
 		private V value;
@@ -23,12 +27,19 @@ public class PriorityQueue<K extends Comparable<K>, V> {
 		}
 	}
 
+	public PriorityQueue()
+	{
+		this.nbElements = 0;
+		this.heap = new Node[0];
+	}
+
 	@requires 	({	"key != null",
-					"key.getClass() == K",
-					"value.getClass() == V"
+					"value != null",
+					"key.getClass().equals(K.class)",
+					"value.getClass().equals(V.class)"
 				})
 	@ensures	({	"$this.nbElements == $old($this.nbElements) + 1",
-					"this.heap.contains(new Node(key, value))"
+					"$this.contains(value) = $old($this.contains(value)) + 1"
 				})
 	public void insert(K key, V value) {
 		Node insertNode = new Node(key, value);
@@ -37,15 +48,31 @@ public class PriorityQueue<K extends Comparable<K>, V> {
 	@requires 	({	"$this.nbElements != 0",
 				})
 	@ensures	({	"$this.nbElements == $old($this.nbElements) - 1",
+					"!$this.contains($old($this.min()))",
+					"$result != null"
 				})
 	public V remove() {
 		return null;
 	}
-	
+
+	@requires 	({	"$this.nbElements != 0",
+				})
+	@ensures	({	"$this.nbElements == $old($this.nbElements)",
+					"$result != null"
+				})
 	public Node min() {
 		return heap[0];
 	}
-	
+
+	private int contains(V value)
+	{
+		int counter = 0;
+		for (Node node:this.heap)
+			if (node.getValue().equals(value))
+				counter++;
+		return counter;
+	}
+
 	private int parent(int i) { 
 		return ((i-1)/2);
 	}
@@ -70,6 +97,10 @@ public class PriorityQueue<K extends Comparable<K>, V> {
 		Node temp = heap[a];
 		heap[a] = heap[b];
 		heap[b] = temp;
+	}
+
+	private int getSize(){
+		return heap.length;
 	}
 	
 	private void trickleUp(int elem) {
