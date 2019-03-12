d6a0a4fe01913ca5947b669f1e577a108838ecde
Jon Zlotnik <j.zlotnik@live.ca>
2019-03-11 21:25:47 -0400

    got it to work with jetbrains

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,8 @@
+import be.ac.ua.ansymo.adbc.annotations.requires;
+
 public class Driver {
 
+    @requires("args.length >= 0")
     public static void main (String[] args)
     {
         System.out.println("Hello, World!");
