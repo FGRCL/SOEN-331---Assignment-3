3c6758629a45c6d59a7e6044a0838156e7b36581
FGRCL <Fgrclaberge@gmail.com>
2019-03-12 12:49:36 -0400

    Added trickleUp and settleHeap methods

--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -27,10 +27,11 @@ public class PriorityQueue<K extends Comparable<K>, V> {
 	}
 	
 	private V remove() {
+		return null;
 	}
 	
 	private V min() {
-		
+		return null;
 	}
 	
 	private int parent(int i) { 
@@ -45,9 +46,42 @@ public class PriorityQueue<K extends Comparable<K>, V> {
 		return ((2*i)+2);
 	}
 	
+	private boolean isRoot(int elem) {
+		return elem == 0;
+	}
+	
+	private boolean hasChild(int elem) {
+		return (leftChild(elem)<=(nbElements-1)  || rightChild(elem)<=(nbElements-1));
+	}
+	
 	private void swap(int a, int b) {
 		Node temp = heap[a];
 		heap[a] = heap[b];
 		heap[b] = temp;
+	}
+	
+	private void trickleUp(int elem) {
+		while((heap[elem].getKey().compareTo(heap[parent(elem)].getKey()) < 0 && !isRoot(elem))) {
+			swap(elem, parent(elem));
+			elem = parent(elem);
+		}
+	}
+	
+	private void settleHeap(int elem) {
+		int leftCompare = heap[elem].getKey().compareTo(heap[leftChild(elem)].getKey());
+		int rightCompare = heap[elem].getKey().compareTo(heap[rightChild(elem)].getKey());
+		while(hasChild(elem) && (leftCompare < 0 || rightCompare < 0)){
+			if(leftCompare <= rightCompare) {
+				swap(elem, leftChild(elem));
+				elem = leftChild(elem);
+			}else if(rightCompare < leftCompare) {
+				swap(elem, rightChild(elem));
+				elem = rightChild(elem);
+			}else {
+				System.out.println("that didn't make sense.");
+			}
+			leftCompare = heap[elem].getKey().compareTo(heap[leftChild(elem)].getKey());
+			rightCompare = heap[elem].getKey().compareTo(heap[rightChild(elem)].getKey());
+		}
 	}
 }
